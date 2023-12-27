1c00cc4c799c30d39846724478913937dede0aa1
Avakyan Sasun <savakyan@lab4u.ru>
2023-12-27 16:00:22 +0400

    Added wysiwyg editor

--- local/php_interface/handlers.php
+++ local/php_interface/handlers.php
@@ -1,6 +1,6 @@
 <?php
 
-AddEventHandler("iblock", "OnIBlockPropertyBuildList", ["CustomProperty", "GetCustomProperty"]);
+AddEventHandler("iblock", "OnIBlockPropertyBuildList", ["CustomProperty", "GetUserTypeDescription"]);
 
 class CustomProperty
 {
@@ -9,7 +9,7 @@ class CustomProperty
     /**
      * @return array 'Массив, описывающий кастомное свойство'
      */
-    public static function GetCustomProperty(): array
+    public static function GetUserTypeDescription(): array
     {
         return [
             'USER_TYPE_ID' => 'custom_property',
@@ -17,84 +17,108 @@ class CustomProperty
             'CLASS_NAME' => __CLASS__,
             'DESCRIPTION' => 'Кастомное свойство файл+строка+HTML/text',
             'PROPERTY_TYPE' => \Bitrix\Iblock\PropertyTable::TYPE_STRING,
-            'GetPropertyFieldHtml' => [__CLASS__, 'GetPropertyFieldHtml'],
-//            "GetSettingsHTML" => [__CLASS__, "GetSettingsHTML"],
-//            "PrepareSettings" => [__CLASS__, "PrepareSettings"],
-            'ConvertToDB' => [__CLASS__, "ConvertToDB"],
-            'ConvertFromDB' => [__CLASS__, "ConvertFromDB"],
+            'GetPropertyFieldHtml' => [__CLASS__, 'GetPropertyFieldHtml'], // Открытие карточки элемента инфоблока
+            "GetSettingsHTML" => [__CLASS__, "GetSettingsHTML"], // Открытие настроек свойства в настройках инфоблока
+//            "PrepareSettings" => [__CLASS__, "PrepareSettings"], // Сохранение свойства в настройках инфоблока
+            'ConvertToDB' => [__CLASS__, "ConvertToDB"], // Сохранении свойства в карточке элемента (b_iblock_element_property)
+            'ConvertFromDB' => [__CLASS__, "ConvertFromDB"], // Извлечение свойства в карточку элемента (b_iblock_element_property)
         ];
     }
 
-    /** Вызывается при открытии карточки элемента инфоблока */
     public static function GetPropertyFieldHtml($arProperty, $arValue, $arHtmlControl)
     {
-        $fieldName = htmlspecialcharsbx($arHtmlControl['VALUE']);
+        \CModule::IncludeModule("fileman");
 
         self::setInputsValue($arValue['VALUE']);
-        $html = self::getInputsHtml($fieldName);
-
+        $html = self::getInputsHtml($arHtmlControl);
         return $html;
     }
 
     private static function setInputsValue($value)
     {
         self::$arInputsValue = [
-            'STRING_INPUT' => '',
-            'FILE_INPUT' => '',
-            'TEXTAREA_INPUT' => ''
+            'STRING' => '',
+            'FILE' => '',
+            'TEXTAREA' => []
         ];
 
-        if (!empty($value) && is_array($value)) {
-            if (!empty($value['STRING'])) {
-                self::$arInputsValue['STRING_INPUT'] = $value['STRING'];
-            }
+        if (!is_array($value)) {
+            return;
+        }
 
-            if (!empty($value['FILE'])) {
-                self::$arInputsValue['FILE_INPUT'] = CFile::GetPath($value['FILE']);
-            }
+        if (!empty($value['STRING'])) {
+            self::$arInputsValue['STRING'] = $value['STRING'];
+        }
 
-            if (!empty($value['TEXTAREA'])) {
-                self::$arInputsValue['TEXTAREA_INPUT'] = $value['TEXTAREA'];
-            }
+        if (!empty($value['FILE'])) {
+            self::$arInputsValue['FILE'] = $value['FILE'];
+        }
+
+        if (!empty($value['TEXTAREA'])) {
+            self::$arInputsValue['TEXTAREA'] = $value['TEXTAREA'];
         }
     }
 
-    private static function getInputsHtml($fieldName): string
+    private static function getInputsHtml($arHtmlControl): string
     {
+        $inputBaseName = htmlspecialcharsbx($arHtmlControl['VALUE']);
+
         $html = '<label for="custom-string">Строка:</label><br>
-                <input 
-                style="width: 300px;" 
-                id="' . $fieldName . '" 
-                name="' . $fieldName . '[STRING]" 
+                <input
+                style="width: 300px;"
+                id="custom-string"
+                name="' . $inputBaseName . '[STRING]"
                 type="text"
-                value="' . self::$arInputsValue['STRING_INPUT'] . '">';
+                value="' . self::$arInputsValue['STRING'] . '">';
 
         $html .= '<br><p>Файл:</p>';
-        $html .= '<span class="adm-input-file" id="test1"><span>Добавить файл</span>
-                <input name="' . $fieldName . '[FILE]" 
-                class="typefile adm-designed-file"
-                id="test"
-                size="20" 
-                type="file" value="' . self::$arInputsValue['FILE_INPUT'] . '"
-                onchange="BX.adminFormTools._modified_file_onchange.call(this)"
-                ></span>';
 
-        $html .= '<br><br><label for="custom-textarea">HTML/TEXT:</label><br>';
-        $html .= '<textarea 
-            class="typearea" 
-            id="custom-textarea" 
-            style="width:200px; height:100px;" 
-            name="' . $fieldName . '[TEXTAREA]" 
-            id="custom-textarea"
-        >' . self::$arInputsValue['TEXTAREA_INPUT'] . '</textarea>';
+        $fileInput = CFile::InputFile(
+            $inputBaseName . '[FILE]',
+            20,
+            self::$arInputsValue['FILE'],
+            strFileType: '',
+            bShowFilePath: false
+        );
 
+        $html .= $fileInput . '<br><br>';
+        $html .= '<label for="custom-textarea">HTML/TEXT:</label>';
+        $html .= self::getEditorHtml($arHtmlControl);
         $html .= '<br><br><br>';
 
         return $html;
     }
 
-    /** Вызывается при сохранении свойства в карточке элемента инфоблока в b_iblock_element_property. Сериализуем
-     */
+    private static function getEditorHtml($arHtmlControl)
+    {
+        ob_start();
+        CFileMan::AddHTMLEditorFrame(
+            $arHtmlControl['VALUE'] . '[TEXTAREA][TEXT]',
+            self::$arInputsValue['TEXTAREA']['TEXT'] ?? '',
+            $arHtmlControl['VALUE'] . '[TEXTAREA][TYPE]',
+            self::$arInputsValue['TEXTAREA']['TYPE'] ?? '',
+            array(
+                'height' => 450,
+                'width' => '100%'
+            ),
+            textarea_field: 'data-original-name="' . $arHtmlControl['VALUE'] . '[TEXTAREA][TEXT]"'
+        );
+
+        ?>
+        <script>
+            window.addEventListener('load', function() {
+                let arTextareas = document.querySelectorAll('.typearea')
+
+                for (let textarea of arTextareas) {
+                    textarea.name = textarea.dataset.originalName;
+                }
+            });
+        </script>
+
+        <?php
+        return ob_get_clean();
+    }
+
     public static function ConvertToDB($arProperty, $arValue)
     {
         $value = $arValue['VALUE'];
@@ -103,25 +127,32 @@ class CustomProperty
         if (
             empty($value['STRING']) &&
             empty($value['FILE']['name']) &&
-            empty($value['TEXTAREA'])
+            empty($value['TEXTAREA']['TEXT'])
         ) {
             return;
         }
 
-        $fileId = CFile::SaveFile($value['FILE'], 'custom-property');
+        if (!empty($value['FILE']['name'])) {
+            $fileId = CFile::SaveFile($value['FILE'], 'custom-property');
+            $arValue['VALUE']['FILE'] = $fileId;
+        }
 
-        $arValue['VALUE']['FILE'] = $fileId;
         $arValue["VALUE"] = serialize($arValue["VALUE"]);
 
         return $arValue;
     }
 
-    /** Вызывается при извлечении свойства в карточку элемента инфоблока из b_iblock_element_property. Десериализуем
-     */
     public static function ConvertFromDB($arProperty, $arValue)
     {
         $arValue["VALUE"] = unserialize($arValue["VALUE"]);
 
         return $arValue;
+    }
+
+    public static function GetSettingsHTML($arProperty, $strHTMLControlName, &$arPropertyFields)
+    {
+        $arPropertyFields = array(
+            "HIDE" => array('DEFAULT_VALUE'),
+        );
     }
 }
--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,3 +1,6 @@
 <?php
 
 require_once $_SERVER["DOCUMENT_ROOT"] . '/local/php_interface/handlers.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/vendor/autoload.php';
+
+\Bitrix\Main\Loader::includeModule('fileman');
